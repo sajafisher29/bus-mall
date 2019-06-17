2406dee50623cde201ee4258aff34352da3e3283
strangestbrew <thomas.lyndsey@gmail.com>
2019-06-17 14:29:58 -0700

    random images look GUD

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var image = document.getElementById('img');
+
+img.addEventListener('click', getRandomImage);
+
+var imagesArray = [
+  'images/bag.jpg',
+  'images/banana.jpg',
+  'images/bathroom.jpg',
+  'images/boots.jpg',
+  'images/breakfast.jpg',
+  'images/bubblegum.jpg',
+  'images/chair.jpg',
+  'images/cthulhu.jpg',
+  'images/dog-duck.jpg',
+  'images/dragon.jpg',
+  'images/pen.jpg',
+  'images/pet-sweep.jpg',
+  'images/scissors.jpg',
+  'images/shark.jpg',
+  'images/sweep.png',
+  'images/tauntaun.jpg',
+  'images/unicorn.jpg',
+  'images/usb.gif',
+  'images/water-can.jpg',
+  'images/wine-glass.jpg',
+];
+
+console.log(imagesArray);
+
+function getRandomImage() {
+  var randomImageIndex = Math.floor(Math.random()*imagesArray.length);
+  var imageSource = imagesArray[randomImageIndex];
+  image.src = imageSource;
+}
+
+getRandomImage();
