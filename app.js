cdf4f3c0d9ced7de36bf0dd96a40712823db5870
strangestbrew <thomas.lyndsey@gmail.com>
2019-06-17 15:46:44 -0700

    three images generating on click, random function semi working

--- app.js
+++ app.js
@@ -1,42 +1,55 @@
 'use strict';
 
 
-var image = document.getElementsByClassName('randomImage');
-randomImage.addEventListener('click', getRandomImage);
+var left = document.getElementById('img1');
+img1.addEventListener('click', getRandomImage);
+
+var center = document.getElementById('img2');
+img2.addEventListener('click', getRandomImage);
+
+var right = document.getElementById('img3');
+img3.addEventListener('click', getRandomImage);
+
 
 var imagesArray = [
-  'images/bag.jpg',
-  'images/banana.jpg',
-  'images/bathroom.jpg',
-  'images/boots.jpg',
-  'images/breakfast.jpg',
-  'images/bubblegum.jpg',
-  'images/chair.jpg',
-  'images/cthulhu.jpg',
-  'images/dog-duck.jpg',
-  'images/dragon.jpg',
-  'images/pen.jpg',
-  'images/pet-sweep.jpg',
-  'images/scissors.jpg',
-  'images/shark.jpg',
-  'images/sweep.png',
-  'images/tauntaun.jpg',
-  'images/unicorn.jpg',
-  'images/usb.gif',
-  'images/water-can.jpg',
-  'images/wine-glass.jpg',
+  './images/bag.jpg',
+  './images/banana.jpg',
+  './images/bathroom.jpg',
+  './images/boots.jpg',
+  './images/breakfast.jpg',
+  './images/bubblegum.jpg',
+  './images/chair.jpg',
+  './images/cthulhu.jpg',
+  './images/dog-duck.jpg',
+  './images/dragon.jpg',
+  './images/pen.jpg',
+  './images/pet-sweep.jpg',
+  './images/scissors.jpg',
+  './images/shark.jpg',
+  './images/sweep.png',
+  './images/tauntaun.jpg',
+  './images/unicorn.jpg',
+  './images/usb.gif',
+  './images/water-can.jpg',
+  './images/wine-glass.jpg',
 ];
 
 console.log(imagesArray);
 
-function getRandomImage() {
-  var randomImageIndex = Math.floor(Math.random()*imagesArray.length);
-  var imageSource = imagesArray[randomImageIndex];
-  img1.src = imageSource;
+function getRandomNumber() {
+    return Math.floor(Math.random()*imagesArray.length);
 }
 
-getRandomImage(img1);
-getRandomImage(img2);
-getRandomImage(img3);
+
+function getRandomImage() {
+  var imageSource = imagesArray[getRandomNumber()];
+  console.log(getRandomNumber());
+  left.src = imageSource;
+  center.src = imageSource;
+  right.src = imageSource;
+}
+
+
+getRandomImage();
 
 
